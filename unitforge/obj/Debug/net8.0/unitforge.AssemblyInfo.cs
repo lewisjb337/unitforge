b2125ab198536794b53f85ad38d5cc5e39eba34f
Lewis <lewisjohnbaxter@gmail.com>
2025-03-08 22:48:41 +0000

    Testing select placeholder logic

--- unitforge/obj/Debug/net8.0/unitforge.AssemblyInfo.cs
+++ unitforge/obj/Debug/net8.0/unitforge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("unitforge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e3e76437dbf524902b12c7e34f6ac6893765b0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3154c6a94b82b5904e39f587aeecc765ebb9848")]
 [assembly: System.Reflection.AssemblyProductAttribute("unitforge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("unitforge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
