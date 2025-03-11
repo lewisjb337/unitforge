327e16130bdbcc59887762c48013cc813ec12a2a
Lewis <lewisjohnbaxter@gmail.com>
2025-03-11 14:51:13 +0000

    Further SEO improvement

--- unitforge/obj/Debug/net8.0/unitforge.AssemblyInfo.cs
+++ unitforge/obj/Debug/net8.0/unitforge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("unitforge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+609cf8570a2182985b7623f434f9d2a6feacf032")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+397076dc71d92e5b20ccd32c63d538a66c48ca8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("unitforge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("unitforge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
